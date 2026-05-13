71b9117b57727a556fe95e90ca7ed64612258dfc
Kaustubh Ukey <ukeykaustubh7@gmail.com>
2026-05-13 14:40:06 +0530

    fix cors for production

--- backend/app.js
+++ backend/app.js
@@ -9,9 +9,13 @@ import imageRoutes from "./routes/imageRoutes.js";
 const app = express(); //Creates backend server object.
 
 app.use(cors({
-  origin:
-  "ai-social-media-content-pipeline.vercel.app",
-  credentials:true,
+ origin: [
+    "https://ai-social-media-content-pipeline.vercel.app",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 })); //Allows requests from frontend.
 
 app.use(express.json()); //Allows backend to read JSON request body.
