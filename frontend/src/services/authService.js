94792b351cff2bf4273025df3a870d2ee074e399
Kaustubh Ukey <ukeykaustubh7@gmail.com>
2026-05-13 13:55:02 +0530

    routes

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://ai-social-media-content-pipeline-81g3.onrender.com";
+const API = "https://ai-social-media-content-pipeline-81g3.onrender.com/api/auth";
 
 export const registerUser = async (userData) => {
 
